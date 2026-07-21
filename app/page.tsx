ad6af209bfe4364568f65035aea0852e495c7702
ximbax <71418223+Sebas1524@users.noreply.github.com>
2026-07-21 11:19:48 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,7 @@ import Testimonials from "@/components/sections/Testimonials";
 export default function Home() {
   return (
     <>
-      {/* Banner arriba de todo */}
-      <div className="w-full">
-        <img
-          src="/BANNER.jpeg"
-          alt="ORTOPEDIX - Productos ortopédicos y rehabilitación"
-          className="w-full h-auto"
-        />
-      </div>
-
-      {/* Menú de navegación (asumo que ya lo tienes en un layout o componente) */}
-      
-      {/* Pop-ups promocionales */}
       <PromoModal />
-
-      {/* Secciones */}
       <Hero />
       <Benefits />
       <FeaturedProducts />
