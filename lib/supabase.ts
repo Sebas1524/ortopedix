f0deba13293c08b005c8e4ad8d53932b78d4e286
ximbax <71418223+Sebas1524@users.noreply.github.com>
2026-08-29 18:37:47 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://skqnunkbnnlsykfnpgcy.supabase.co";
-const supabaseAnonKey = "sb_publishable_XioCnx1jeJNgRQHAPTM4Pg_AdlllZyc";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrcW51bmtibm5sc3lrZm5wZ2N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgwMjYzNDUsImV4cCI6MjEwMzYwMjM0NX0.CmE2x7iZz2LO96hDhub2iS7ml4Q3RSwW5h73COYI1ao";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
