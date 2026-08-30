25ed4908ac206418619f1fb9b4db59e833ceff9c
ximbax <71418223+Sebas1524@users.noreply.github.com>
2026-08-30 12:08:02 -0500

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -14,9 +14,9 @@ export const siteConfig = {
   email: "info@ortopedix.com",
   phone: "+593 99 838 5165",
   address: "Gabriel Garcia Moreno y Federico Gonzalez S1-80, 170060 Quito",
-  mapUrl: "https://maps.app.goo.gl/ywGvoNovNjYtnsgcA",
+  mapUrl: "https://maps.app.goo.gl/mqaEQeBPwvNN8BsRA",
   mapEmbed:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.793!2d-78.5249!3d-0.2295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMDDCsDEzJzQ2LjIiUyA3OMKwMzEnMjkuNiJX!5e0!3m2!1ses!2sec!4v1620000000000!5m2!1ses!2sec",
+    "https://maps.google.com/maps?q=Gabriel+Garcia+Moreno+y+Federico+Gonzalez+S1-80,+Quito,+Ecuador&t=&z=16&ie=UTF8&iwloc=&output=embed",
 
   // ---- HORARIOS ----
   hours: [
